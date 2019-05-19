0b27dff1147a6b330bbff6a32a0800097defe465
rinithprasad <rinithprasad@tamu.edu>
2019-05-19 15:04:21 -0500

    new repo

--- src/main/java/com/samples/ocjp8/part1/two/TestShapes.java
+++ src/main/java/com/samples/ocjp8/part1/two/TestShapes.java
@@ -3,8 +3,13 @@ package com.samples.ocjp8.part1.two;
 public class TestShapes {
     public static void main (String[] args){
         PlayerPiece shape = new PlayerPiece();
+        TilePiece tile = new TilePiece();
+        doShapes(shape);
+        doShapes(tile);
+
+    }
+    public static void doShapes (GameShape shape){
         shape.displayShape();
-        shape.movePiece();
     }
 }
 class GameShape{
@@ -15,5 +20,11 @@ class GameShape{
 class PlayerPiece extends GameShape {
     public void movePiece(){
         System.out.println("moving game pieces");
+    }
+}
+
+class TilePiece extends GameShape{
+    public void getAdjacent(){
+        System.out.println("getting adjacent tiles");
     }
 }
