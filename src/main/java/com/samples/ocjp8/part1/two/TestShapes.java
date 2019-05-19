228cfe5d21b59b60fa50bab89bde45b698833331
rinithprasad <rinithprasad@tamu.edu>
2019-05-19 14:59:09 -0500

    new commit

---
+++ src/main/java/com/samples/ocjp8/part1/two/TestShapes.java
@@ -0,0 +1,19 @@
+package com.samples.ocjp8.part1.two;
+
+public class TestShapes {
+    public static void main (String[] args){
+        PlayerPiece shape = new PlayerPiece();
+        shape.displayShape();
+        shape.movePiece();
+    }
+}
+class GameShape{
+    public void displayShape(){
+        System.out.println("displaying shape");
+    }
+}
+class PlayerPiece extends GameShape {
+    public void movePiece(){
+        System.out.println("moving game pieces");
+    }
+}
